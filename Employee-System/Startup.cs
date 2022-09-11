17bb35d612db343a4efb506f840004bada4f62fc
8080106150 <mandarshitole27@gmail.com>
2022-09-11 12:10:49 +0530

    Added Student Data

--- Employee-System/Startup.cs
+++ Employee-System/Startup.cs
@@ -29,6 +29,7 @@ namespace Employee_System
             services.AddControllers();
 
             services.AddSingleton<IEmployeeData,MockEmployeeData>();
+            services.AddSingleton<IStudentData,MockStudentData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
